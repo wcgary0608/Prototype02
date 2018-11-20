6e69d3819d356bc448169ee1533ba3ffe468e12e
jxue <543563370@qq.com>
2018-11-20 15:58:26 +0800

    Complete UIManagers files

--- Prototype/Assets/Scripts/MainScene/UIManagers/CardMenuUI.cs
+++ Prototype/Assets/Scripts/MainScene/UIManagers/CardMenuUI.cs
@@ -1,7 +1,39 @@
-﻿public class CardMenuUI : IUserInterface
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardMenuUI : IUserInterface
 {
+    private Button _btnAllocate;
+
+    private Button _btnCompose;
+
+    private GameObject _oAllocationPanel;
+
+    private GameObject _oCardGroupContent;
+
+    private TextMeshProUGUI _tCurGroupCount;
+
+    private Button _btnReturn;
+
+    private Button _btnAddGroup;
+
+    private Button _btnDeleteGroup;
+
+    private GameObject _oCurGroupContent;
+
+    private TextMeshProUGUI _tTotalCardCount;
+
+    private TextMeshProUGUI _tCurGroupName;
+
+    private GameObject _oAllCardContent;
+
     public CardMenuUI(MainSceneTreeNodeManager center) : base(center)
     {
+        GetUIComponents();
+        HideRootUI();
     }
 
     public override void Initialize()
@@ -22,5 +54,39 @@
     public override void FixedUpdate()
     {
         base.FixedUpdate();
+    }
+
+    private void GetUIComponents()
+    {
+        //Temporary Variables
+        var MenuUI = UITool.FindUIGameObject("MenuUI");
+        var cardGroupList = UnityTool.FindChildGameObject(_oAllocationPanel, "cardGroupList");
+        var cardGroupViewport = UnityTool.FindChildGameObject(cardGroupList, "Viewport");
+        var curGroupList = UnityTool.FindChildGameObject(_oAllocationPanel, "curGroupList");
+        var curGroupViewport = UnityTool.FindChildGameObject(curGroupList, "Viewport");
+        var allCardList = UnityTool.FindChildGameObject(m_RootUI, "allCardList");
+        var allCardViewport = UnityTool.FindChildGameObject(allCardList, "Viewport");
+
+
+        //UI Components
+        m_RootUI = UnityTool.FindChildGameObject(MenuUI, "CardMenu");
+
+        _btnAllocate = UITool.GetUIComponent<Button>(m_RootUI, "allocateBtn");
+        Debug.Log(_btnAllocate.name);
+        _btnCompose = UITool.GetUIComponent<Button>(m_RootUI, "composeBtn");
+
+        _oAllocationPanel = UnityTool.FindChildGameObject(m_RootUI, "allocateCardPanel");
+        _oCardGroupContent = UnityTool.FindChildGameObject(cardGroupViewport, "Content");
+        _tCurGroupCount = UITool.GetUIComponent<TextMeshProUGUI>(_oAllocationPanel, "curGroupCount");
+        _btnReturn = UITool.GetUIComponent<Button>(_oAllocationPanel, "returnBtn");
+        _btnAddGroup = UITool.GetUIComponent<Button>(_oAllocationPanel, "addGroupBtn");
+        _btnDeleteGroup = UITool.GetUIComponent<Button>(_oAllocationPanel, "deleteGroupBtn");
+        _oCurGroupContent = UnityTool.FindChildGameObject(curGroupViewport, "Content");
+
+        _tTotalCardCount = UITool.GetUIComponent<TextMeshProUGUI>(m_RootUI, "totalCardCount");
+        _tCurGroupName = UITool.GetUIComponent<TextMeshProUGUI>(m_RootUI, "curGroupName");
+
+        _oAllCardContent = UnityTool.FindChildGameObject(allCardViewport, "Content");
+
     }
 }
--- Prototype/Assets/Scripts/MainScene/UIManagers/SocialMenuUI.cs
+++ Prototype/Assets/Scripts/MainScene/UIManagers/SocialMenuUI.cs
@@ -1,5 +1,42 @@
-﻿public class SocialMenuUI : IUserInterface
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SocialMenuUI : IUserInterface
 {
+    private Button _btnAll;
+
+    private Button _btnFriend;
+
+    private Button _btnNormal;
+
+    private Button _btnEnermy;
+
+    private GameObject _oNpcContent;
+
+    //NPC Detail Panel
+    private GameObject _oNpcDetailPanel;
+
+    private Image _imgCharacter;
+
+    private Button _btnReturnToRoot;
+
+    private TextMeshProUGUI _tDescription;
+
+    private TextMeshProUGUI _tSideStatus;
+
+    private TextMeshProUGUI _tGroupName;
+
+    private TextMeshProUGUI _tFriendshipValue;
+
+    private GameObject _oNeiGongContent;
+
+    private GameObject _oCardContent;
+
+    private GameObject _oInfoContent;
+
     public SocialMenuUI(MainSceneTreeNodeManager center) : base(center)
     {
     }
@@ -23,4 +60,44 @@
     {
         base.FixedUpdate();
     }
+
+    private void GetUIComponents()
+    {
+        //Temporary Variables
+        var MenuUI = UITool.FindUIGameObject("MenuUI");
+        var npcList = UnityTool.FindChildGameObject(m_RootUI, "npcList");
+        var npcViewport = UnityTool.FindChildGameObject(m_RootUI, "Viewport");
+
+        var neiGongList = UnityTool.FindChildGameObject(_oNpcDetailPanel, "neiGongList");
+        var neiGongViewport = UnityTool.FindChildGameObject(neiGongList, "Viewport");
+        var cardList = UnityTool.FindChildGameObject(_oNpcDetailPanel, "cardList");
+        var cardViewport = UnityTool.FindChildGameObject(cardList, "Viewport");
+        var infoList = UnityTool.FindChildGameObject(_oNpcDetailPanel, "infoList");
+        var infoViewport = UnityTool.FindChildGameObject(infoList, "Viewport");
+
+
+        //UI Components
+        m_RootUI = UnityTool.FindChildGameObject(MenuUI, "SocialMenu");
+
+        _btnAll = UITool.GetUIComponent<Button>(m_RootUI, "allBtn");
+        _btnFriend = UITool.GetUIComponent<Button>(m_RootUI, "friendBtn");
+        _btnNormal = UITool.GetUIComponent<Button>(m_RootUI, "normalBtn");
+        _btnEnermy = UITool.GetUIComponent<Button>(m_RootUI, "enermyBtn");
+
+        _oNpcContent = UnityTool.FindChildGameObject(npcViewport, "Content");
+
+        _oNpcDetailPanel = UnityTool.FindChildGameObject(m_RootUI, "npcDetailPanel");
+        _imgCharacter = UITool.GetUIComponent<Image>(_oNpcDetailPanel, "characterImage");
+        _btnReturnToRoot = UITool.GetUIComponent<Button>(_oNpcDetailPanel, "returnToRootBtn");
+        _tDescription = UITool.GetUIComponent<TextMeshProUGUI>(_oNpcDetailPanel, "description");
+        _tSideStatus = UITool.GetUIComponent<TextMeshProUGUI>(_oNpcDetailPanel, "sideStatus");
+        _tGroupName = UITool.GetUIComponent<TextMeshProUGUI>(_oNpcDetailPanel, "groupName");
+        _tFriendshipValue = UITool.GetUIComponent<TextMeshProUGUI>(_oNpcDetailPanel, "friendshipValue");
+
+        _oNeiGongContent = UnityTool.FindChildGameObject(neiGongViewport, "Content");
+        _oCardContent = UnityTool.FindChildGameObject(cardViewport, "Content");
+        _oInfoContent = UnityTool.FindChildGameObject(infoViewport, "Content");
+
+    }
+
 }
